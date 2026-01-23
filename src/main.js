5e2e5e1df6b89688c0ebfa62ab184bc75a64dd2d
舟 <Robin.wangyuezhou@edu.yungu.org>
2026-01-23 08:06:17 +0800

    Update Analytics SDK initialization URL

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ app.use(router)
 
 // 初始化 Analytics SDK
 // eslint-disable-next-line no-undef
-Analytics.init('http://127.0.0.1:8888', { autoTrackPageView: true, skipInitialPageView: true })
+Analytics.init('http://127.0.0.1:5217', { autoTrackPageView: true, skipInitialPageView: true })
 
 // 等路由 ready 后再绑定并发送首屏 PV，避免刷新时先被记录为 '/'
 router.isReady().then(() => {
